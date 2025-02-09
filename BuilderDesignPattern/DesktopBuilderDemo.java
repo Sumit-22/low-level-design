a5caf169289227ca91708fa3d8a56b2a91db2afc
Sumit Kumar <145086126+Sumit-22@users.noreply.github.com>
2025-02-09 13:33:39 +0000

    Updated code with new changes

--- BuilderDesignPattern/DesktopBuilderDemo.java
+++ BuilderDesignPattern/DesktopBuilderDemo.java
@@ -1,7 +1,13 @@
 public class DesktopBuilderDemo{
     public static void main(String[] args) {
         DesktopDirector director  =  new DesktopDirector();
-
+        DellDesktopBuilder dellDesktopBuilder = new DellDesktopBuilder();
+        Desktop dellDesktop = director.buildDesktop(dellDesktopBuilder);
          
+        HPDesktopBuilder hpDesktopBuilder = new HPDesktopBuilder();
+        Desktop hpDesktop = director.buildDesktop(hpDesktopBuilder);
+
+        dellDesktop.display();
+        hpDesktop.display();
     }
 }
