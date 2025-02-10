0a7f19b5ed904fe34a492acc4ea610870081aa11
Sumit Kumar <145086126+Sumit-22@users.noreply.github.com>
2025-02-10 09:02:10 +0000

    Updated code with new changes

---
+++ SingletonDesignPattern/differentSingletonImpl.java
@@ -0,0 +1,15 @@
+/*
+ * Key Concepts of Singleton Pattern:
+
+    Single Instance:
+        Ensures that only one object of the class exists.
+
+    Global Access:
+        Provides a way to access the single instance globally without creating multiple instances.
+
+    Lazy Initialization (Optional):
+        The instance is created only when it is required (on-demand).
+
+    Thread-Safety Considerations:
+        In a multi-threaded environment, proper synchronization is required to avoid multiple instance creation.
+ */
