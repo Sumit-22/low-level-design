f0954406a64de5ae3156990c615d0d6624d96304
Sumit Kumar <145086126+Sumit-22@users.noreply.github.com>
2025-02-12 19:28:32 +0000

    Updated code with new changes

--- Adapter Design Pattern/XmlToJson.java
+++ Adapter Design Pattern/XmlToJson.java
@@ -5,8 +5,8 @@ class JSONAnalyticsTool {
         this.jsonData = jsonData;
     }
 
-    public void AnalyzeData() {
-        if (jsonData.contains("json")) {
+    public void analyzeData() {
+        if (jsonData != null && jsonData.contains("json")) {
             System.out.println("Analyzing JSON Data - " + jsonData);
         } else {
             System.out.println("Not in the correct format. Can't analyze!");
@@ -15,7 +15,7 @@ class JSONAnalyticsTool {
 }
 
 interface AnalyticsTool {
-    void AnalyzeData();
+    void analyzeData();
 }
 
 class XMLToJSONAdapter implements AnalyticsTool {
@@ -23,26 +23,35 @@ class XMLToJSONAdapter implements AnalyticsTool {
 
     public XMLToJSONAdapter(String xmlData) {
         System.out.println("Converting the XML Data '" + xmlData + "' to JSON Data!");
-        String newData = xmlData + " in json";
+        String newData = convertXmlToJson(xmlData);
         jsonAnalyticsTool = new JSONAnalyticsTool();
         jsonAnalyticsTool.setJsonData(newData);
     }
 
-    public void AnalyzeData() {
-        jsonAnalyticsTool.AnalyzeData();
+    private String convertXmlToJson(String xmlData) {
+        // Simulating XML to JSON conversion (in real-world, use a library like org.json or Jackson)
+        return "{ \"data\": \"" + xmlData + "\" } in json";
+    }
+
+    @Override
+    public void analyzeData() {
+        jsonAnalyticsTool.analyzeData();
     }
 }
 
 public class XmlToJson {
     public static void main(String[] args) {
         String xmlData = "Sample Data";
+
+        // Directly analyzing JSON data (incorrect format case)
         JSONAnalyticsTool tool1 = new JSONAnalyticsTool();
         tool1.setJsonData(xmlData);
-        tool1.AnalyzeData();
+        tool1.analyzeData();
 
         System.out.println("----------------------------------------------");
 
+        // Using Adapter to convert XML to JSON and analyze it
         AnalyticsTool tool2 = new XMLToJSONAdapter(xmlData);
-        tool2.AnalyzeData();
+        tool2.analyzeData();
     }
 }
